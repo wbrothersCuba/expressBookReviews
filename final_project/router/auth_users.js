9cd231b8b485d71077c3361fb8a2b133c00e6b2a
wbrothersCuba <yanet.wilmar@gmail.com>
2026-02-11 08:26:23 -0500

    Implemented auth_users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,24 +5,41 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
+    return users.some((user)=>user.username === usernameuser)
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username, password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
+    let valid = user.filter((user)=>user.username === username);
+    if(valid.length > 0){
+        return true;   
+    }else{
+        return false;
+    }
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({ message: "Body Empty" });
+    }
+    let accesToken = jwt.sign({
+        data: user
+    }, 'access', { expiresIn: 60 * 60 });
+    req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.authenticated = regd_users;
